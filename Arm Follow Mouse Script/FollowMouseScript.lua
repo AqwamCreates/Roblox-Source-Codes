9edfb6fb947baaabe4627567a30ef9b008a5c5ce
AqwamCreates <67371914+AqwamCreates@users.noreply.github.com>
2023-05-08 23:38:18 +0800

    Update FollowMouseScript.lua

--- Arm Follow Mouse Script/FollowMouseScript.lua
+++ Arm Follow Mouse Script/FollowMouseScript.lua
@@ -1,41 +1,103 @@
-local MouseFollowFunctionRemoteEvent = game:GetService("ReplicatedStorage").Events.MouseFollowFunctionRemoteEvent
-
-local armRotationRate = 0.01
-local offset = 0.5
-
 local Character = script.Parent
+local Player = game:GetService("Players"):GetPlayerFromCharacter(Character)
 
-local Player = game.Players:GetPlayerFromCharacter(Character)
+local TweenService = game:GetService("TweenService")
+
+local tweenInfo = TweenInfo.new(
+	0.1, -- Time
+	Enum.EasingStyle.Linear, -- EasingStyle
+	Enum.EasingDirection.Out, -- EasingDirection
+	0, -- RepeatCount (when less than zero the tween will loop indefinitely)
+	false, -- Reverses (tween will reverse once reaching it's goal)
+	0 -- DelayTime
+)
+
+local Events = game:GetService("ReplicatedStorage").Events
+local MouseFollowFunctionRemoteEvent = Events:WaitForChild("MouseFollowFunctionRemoteEvent")
+local AimRemoteEvent = Events.AimRemoteEvent
 
 local RightUpperArm = Character.RightUpperArm
 local RightShoulder = RightUpperArm.RightShoulder
 
-local function rotateShoulder(Shoulder, angle)
-	
-	local ShoulderC0 = Shoulder.C0
-	
-	local ShoulderC0Vector = ShoulderC0.Position
-	
-	Shoulder.C0 = CFrame.new(ShoulderC0Vector) * CFrame.Angles(angle, 0, 0) 
-	
+local LeftUpperArm = Character.LeftUpperArm
+local LeftShoulder = LeftUpperArm.LeftShoulder
+
+local angleXWhenAimingInRadians = math.rad(50) -- add positive value to current value to move left, add negative value to current value to move right. At 46, it alligns properly when in full screen. Same for 50
+
+local rightArmAngleWhenNotAimingInRadians = math.rad(-30)
+local leftArmAngleWhenNotAimingInRadians = math.rad(60)
+
+local Tool
+local PreviousTool
+local DefaultToolGripCFrame
+
+local function rotateShoulderC0(Shoulder, angleX)
+
+	local goal = {}
+
+	goal.C0 = CFrame.new(Shoulder.C0.Position) * CFrame.Angles(angleX, 0, 0) 
+
+	local tween = TweenService:Create(Shoulder, tweenInfo, goal)
+
+	tween:Play()
+
+end
+
+local function rotateShoulderC0Coroutine(Shoulder, angleX)
+
+	coroutine.resume(coroutine.create(function()
+
+		rotateShoulderC0(Shoulder, angleX)
+
+	end))
+
+end
+
+local function onMouseFollowEvent(ReceivedPlayer, angleYInRadians)
+
+	if (ReceivedPlayer ~= Player) then return nil end
+
+
+	if Tool then
+
+		rotateShoulderC0Coroutine(RightShoulder, angleYInRadians)
+
+	elseif (Tool == nil) then
+
+		rotateShoulderC0Coroutine(RightShoulder, 0)
+
+	end
+
 end
 
 
-MouseFollowFunctionRemoteEvent.OnServerEvent:Connect(function(CurrentPlayer, angle)
-	
-	if (CurrentPlayer ~= Player) then return nil end
-	
-	local Tool = Character:FindFirstChildWhichIsA("Tool")
-	
-	if Tool then
-		
-		rotateShoulder(RightShoulder, angle)
-		
-	else
-		
-		rotateShoulder(RightShoulder, 0)
-		
-	end
-	
+local function checkIfToolAddedToCharacter(Child)
 
-end)
+	if not (Child:IsA("Tool")) then return nil end
+
+	task.wait(0.01)
+
+	PreviousTool = Tool
+
+	Tool = Child
+
+	DefaultToolGripCFrame = Child.Grip
+
+end
+
+local function checkIfToolRemovedOrChangedFromCharacter(Child)
+
+	if not (Child:IsA("Tool")) then return nil end
+
+	PreviousTool = Tool
+
+	PreviousTool.Grip = DefaultToolGripCFrame
+
+	Tool = nil
+
+end
+
+Character.ChildAdded:Connect(checkIfToolAddedToCharacter)
+Character.ChildRemoved:Connect(checkIfToolRemovedOrChangedFromCharacter)
+
+MouseFollowFunctionRemoteEvent.OnServerEvent:Connect(onMouseFollowEvent)
